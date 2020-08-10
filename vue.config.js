49c50b422af37e3d97978ac4e29cddfececbdc89
Starlog <785276471@qq.com>
2020-08-10 15:04:51 +0800

    修改vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,12 @@ const path = require('path');//引入path模块
 function resolve(dir) {
     return path.join(__dirname, dir)//path.join(__dirname)设置绝对路径
 }
+
+
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/supermail/'
+    : '/',
     chainWebpack: (config) => {
         config.resolve.alias
             .set('@', resolve('./src'))
