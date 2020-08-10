9a5ac6f9e76a94f578961a41c0342439ca74ef68
Starlog <785276471@qq.com>
2020-08-10 16:30:23 +0800

    部署

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/supermail/'
+    ? '/supermail'
     : '/',
     chainWebpack: (config) => {
         config.resolve.alias
